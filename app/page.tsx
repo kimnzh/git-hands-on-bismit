3487e86ca6d0c8b431634e1c7258ea10343d037e
vimooooo <violinmonica190207@gmail.com>
2025-10-31 17:44:41 +0700

    testing feature

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center bg-linear-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
         <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Hands on to <span className="text-[hsl(280,100%,70%)]">Git</span>
+          Hands on to <span className="text-[hsl(300,100%,70%)]">Git</span>
         </h1>
       </div>
     </main>
