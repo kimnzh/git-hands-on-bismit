3192516f27188ba9e213609e1d6032586da3f96d
Fayyad M Madani <fayyadmadani.11@gmail.com>
2025-10-31 17:47:32 +0700

    feat: create page-fayyad & tictactoe

---
+++ app/page-fayyad/page.tsx
@@ -0,0 +1,10 @@
+export default function NamaSiswaPage() {
+      return (
+    <main className="flex justify-center items-center min-h-screen bg-linear-to-b from-[#2e026d] to-[#15162c] text-white p-6">
+      <div className="w-full max-w-md">
+        <h1 className="text-3xl md:text-4xl font-bold text-center mb-6 justify-center">Tabel Nama Siswa</h1>
+      </div>
+      
+    </main>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -7,7 +8,20 @@ export default function Home() {
         <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
           Hands on to <span className="text-[hsl(280,100%,70%)]">Git</span>
         </h1>
+        <h2 className="text-4xl font-bold">
+          Testing Doang <span className="text-cyan-300">!!</span>
+        </h2>
       </div>
+      <Link href="/page-fayyad" 
+      className="px-6 py-3 bg-white/10 hover:bg-white/20 border border-white/20 rounded-xl font-medium transition"
+      >
+        Go to Nama Siswa
+      </Link>
+      <Link href="/tictactoe" 
+      className="mt-4 px-6 py-3 bg-white/10 hover:bg-white/20 border border-white/20 rounded-xl font-medium transition"
+      >
+        Go to Tic-Tac-Toe
+      </Link>
     </main>
   );
 }
